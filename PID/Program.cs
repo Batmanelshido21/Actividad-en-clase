dfb51c3a43ae8381ebaa6e23ba62610d6f476555
Javier <javierjuarez.jj28@gmail.com>
2020-03-21 22:55:55 -0600

    Actividad en clase
    Actividad en clase

---
+++ PID/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = Process.GetCurrentProcess();
+            Console.WriteLine(process.Id);
+            Console.ReadLine();
+        }
+    }
+}
